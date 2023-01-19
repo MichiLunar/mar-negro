df207c6acde7c43e0d4b2ce3be4d98e3eafdf532
MichiLunar <michidevelop@gmail.com>
2023-01-19 17:34:27 -0600

    Avance parcial del diseño parte 2

--- lib/UI/shared/widgets/contacto_widgets.dart
+++ lib/UI/shared/widgets/contacto_widgets.dart
@@ -8,7 +8,27 @@ class ContactoWidgets extends StatelessWidget {
     return Container(
       width: size.width * .20,
       height: double.infinity,
-      color: Colors.indigo,
+      //color: Colors.indigo,
+      child: Column(
+        children: [
+          SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.mail_outline),
+                iconSize: 20.0,
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.phone_outlined),
+                iconSize: 20.0,
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
